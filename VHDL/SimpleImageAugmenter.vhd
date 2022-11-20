3b464c1431047f89e82b0307809fae5d455aa0ce
Juan Jonathan <juan.jonathan@ui.ac.id>
2022-11-20 12:02:08 +0700

    Add VHDL

---
+++ VHDL/SimpleImageAugmenter.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity SimpleImageAugmenter is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic;
+        
+    );
+end entity SimpleImageAugmenter;
+
+architecture rtl of SimpleImageAugmenter is
+
+begin
+
+    
+
+end architecture;
