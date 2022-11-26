93702193cfb0ef828487aa176602e437ca1d343d
Juan Jonathan <juan.jonathan@ui.ac.id>
2022-11-27 01:14:14 +0700

    Brightness fix

--- VHDL/TypeDeclarations.vhd
+++ VHDL/TypeDeclarations.vhd
@@ -4,10 +4,10 @@ use ieee.std_logic_1164.all;
 package TypeDeclarations is
 
     -- 2 Dimensional Array to store image per pixel (2k x 2k RGB) 
-    type matrix is array(1999 downto 0, 1999 downto 0, 2 downto 0) of Integer range 0 to 255;
+    type matrix is array(1999 downto 0, 1999 downto 0, 2 downto 0) of Integer;
 
     -- Temporary array for (R, G, B) channels
-    type RGB is array(2 downto 0) of integer range 0 to 255;
+    type RGB is array(2 downto 0) of integer;
 
     -- States :
     -- S0 --> Wait for input
